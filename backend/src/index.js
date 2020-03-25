6ed15b465950b1d7f881efa8cc12957e6145b856
Marlon Ourives <marloncicero_araujo@hotmail.com>
2020-03-24 22:25:29 -0400

    Back-end da aplicação

---
+++ backend/src/index.js
@@ -0,0 +1,13 @@
+const express = require('express'); //é um pacote
+const cors = require('cors');
+const routes = require('./routes'); //importa as rotas
+//passa o caminho relativo, para referenciar a mesma pasta do arquivo index "./"
+
+const app = express();
+
+app.use(cors());
+
+app.use(express.json());//informa o express que está usando json, importante vim antes das rotas
+
+app.use(routes);
+app.listen(3333);
