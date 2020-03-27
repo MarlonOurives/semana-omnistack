7f0b42cff0b1068ad9c5f65b078e8a076427993d
Marlon Ourives <marloncicero_araujo@hotmail.com>
2020-03-27 16:13:46 -0400

    Finalizando a semana omnistack 11

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,5 +1,6 @@
 const express = require('express'); //é um pacote
 const cors = require('cors');
+const { errors} = require('celebrate');
 const routes = require('./routes'); //importa as rotas
 //passa o caminho relativo, para referenciar a mesma pasta do arquivo index "./"
 
@@ -10,4 +11,6 @@ app.use(cors());
 app.use(express.json());//informa o express que está usando json, importante vim antes das rotas
 
 app.use(routes);
-app.listen(3333);
+app.use(errors());
+
+module.exports = app;
